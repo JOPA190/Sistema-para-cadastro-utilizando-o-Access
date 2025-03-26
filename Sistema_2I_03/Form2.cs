587545d355b02951b05db404175d1e7fe0181002
JOPA190 <passos.jco@gmail.com>
2025-03-26 12:44:23 -0300

    Adicionar arquivos de projeto.

---
+++ Sistema_2I_03/Form2.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Data.OleDb;
+using Microsoft.Win32;
+
+namespace Sistema_2I_03
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_consultar_Click(object sender, EventArgs e)
+        {
+            string strCon = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\joão\bd_22_banco.mdb";
+
+            try
+            {
+
+                if (txt_Id.Text != string.Empty)
+                {
+                    //Declarar objetos de Classe de Conexão
+                    OleDbConnection conectar = new OleDbConnection(strCon);
+                    //Abrir Objeto de Conexão com Banco de Dados criada acima;
+                    conectar.Open();
+
+                    //Mondando a String (concatenando) com os objetos do Formulário 
+                    string strSQL = "SELECT * FROM contatos WHERE (ID =" + txt_Id.Text + ")";
+
+                    //Criar o Objeto com a classe de Command (comando) para armazenar a Instrução / Comando SQL
+                    OleDbCommand comandoSQL = new OleDbCommand(strSQL, conectar);
+                    //Criar objeto DbDataReader
+                    DbDataReader lerRegistro = comandoSQL.ExecuteReader();
+
+                    //Metodo Read(): Informe um "boolean" "True" (exite Registro) e "False" (Não Existe Registro),
+                    //Possibilita Ler o Proximo Regsitro de uma Tabela (Enquanto for True, Se existir registro)
+
+                    if (lerRegistro.Read())
+                    {
+                        //Populando Objetos do Form com Dados do Registro Lido (lerRegistro)
+                        txt_Nome.Text = lerRegistro.GetString(1);
+                        txt_telefone.Text = lerRegistro.GetString(2);
+                        txt_email.Text = lerRegistro.GetString(3);
+                        txt_Cidade.Text = lerRegistro.GetString(4);
+                        txt_UF.Text = lerRegistro.GetString(5);
+
+                        if (DialogResult.No == MessageBox.Show("Deseja Editar Registro?", "Sistema Informa",
+                                               MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                                               MessageBoxDefaultButton.Button2))
+                        {
+                            //Limpar Objetos do Formulário
+                            LimparDados();
+
+                        }
+                        else
+                        {
+                            //Habilitar botões
+                            btn_excluir.Enabled = true;
+                            btn_alterar.Enabled = true;
+
+                            //Desabilitando Campo CPF
+                            txt_Id.Enabled = false;
+                        }
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Identificador " + txt_Id.Text + " Não Localizado!!!", "Sistema Informa");
+                        txt_Id.Focus();
+                    }
+
+
+
+                    //Fechar Conexão
+                    conectar.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("Preencher o campo Identificador (ID)!!!");
+                }
+
+                //Votar Cursor para o Objeto de Formulario
+                txt_Id.Focus();
+
+
+            }
+
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+        }
+
+        private void LimparDados()
+        {
+            txt_Id.Clear();
+            txt_Nome.Clear();
+            txt_telefone.Clear();
+            txt_email.Clear();
+            txt_Cidade.Clear();
+            txt_UF.Clear();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            btn_alterar.Enabled = false;
+            btn_excluir.Enabled = false;
+        }
+
+        private void Btn_inserir_Click(object sender, EventArgs e)
+        {
+            string strCon = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\joão\bd_22_banco.mdb";
+
+            try
+            {
+                if (txt_Nome.Text != string.Empty)
+                {
+                    //Declarar objetos de Classe de Conexão
+                    OleDbConnection conectar = new OleDbConnection(strCon);
+                    //Abrir Objeto de Conexão com Banco de Dados criada acima;
+                    conectar.Open();
+
+                    //Montando a variavel tipo String "strSQL" de Leitura de Dados (concatenando) com os objetos do Formulário
+                    string strSQL = "SELECT * FROM contatos WHERE (nome =" + "'" + txt_Nome.Text + "')";
+
+                    //Criar o Objeto com a classe de Command (comando) para armazenar a Instrução / Comando SQL
+                    OleDbCommand comandoSQL = new OleDbCommand(strSQL, conectar);
+
+                    DbDataReader lerRegistro = comandoSQL.ExecuteReader();
+
+                    //Metodo Read(): Informe um "boolean" "True" (exite Registro) e "False" (Não Existe Registro),
+                    //Possibilita Ler o Proximo Registro de uma Tabela (Enquanto for True, Se existir registro)
+
+                    if (lerRegistro.Read())
+                    {
+                        MessageBox.Show("Contato (nome) já Existe!!!", "Sistema Informa");
+                    }
+                    else
+                    {
+                        //Montando a variavel tipo String "strSQL" de Inserção dos Dados (concatenando) com os objetos do Formulário 
+                        strSQL = "INSERT INTO contatos (nome,telefone,email,cidade,UF) " +
+                                  "VALUES (" +
+                                  "'" + txt_Nome.Text + "'," +
+                                  "'" + txt_telefone.Text + "'," +
+                                  "'" + txt_email.Text + "'," +
+                                  "'" + txt_Cidade.Text + "'," +
+                                  "'" + txt_UF.Text + "')";
+
+                        //Observe que as MaskTextBox ao utilizar o caracer "." ele inverte para "," (vice-versa)
+
+                        //Criar o Objeto com a classe de Command (comando) para armazenar a Instrução / Comando SQL
+                        //comandoSQL já é uma Classe OleDbCommand, portanto só precisamos Instacia-lá
+                        comandoSQL = new OleDbCommand(strSQL, conectar);
+
+                        //Executando 
+                        comandoSQL.ExecuteNonQuery();
+                        MessageBox.Show("Registro Inserido com Sucesso...", "Sistema Informa");
+
+                        //Limpar Objetos do Formulário
+                        LimparDados();
+
+                    }
+
+                    //Fechar Conexão
+                    conectar.Close();
+
+                }
+                else
+                {
+                    MessageBox.Show("Preencher pelo menos o campo NOME!!!");
+                }
+
+                //Votar Cursor para o Objeto de Formulario
+                txt_Nome.Focus();
+
+
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+
+        }
+
+    }
+    }
+
---
+++ Sistema_2I_03/Form3.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Sistema_2I_03
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_excluir = new System.Windows.Forms.Button();
+            this.btn_alterar = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.btn_inserir = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.txt_Cidade = new System.Windows.Forms.TextBox();
+            this.txt_Id = new System.Windows.Forms.TextBox();
+            this.txt_UF = new System.Windows.Forms.TextBox();
+            this.txt_telefone = new System.Windows.Forms.TextBox();
+            this.txt_Nome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(361, 254);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(61, 72);
+            this.btn_exit.TabIndex = 33;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            // 
+            // btn_excluir
+            // 
+            this.btn_excluir.Location = new System.Drawing.Point(271, 274);
+            this.btn_excluir.Name = "btn_excluir";
+            this.btn_excluir.Size = new System.Drawing.Size(70, 52);
+            this.btn_excluir.TabIndex = 32;
+            this.btn_excluir.Text = "Excluir";
+            this.btn_excluir.UseVisualStyleBackColor = true;
+            // 
+            // btn_alterar
+            // 
+            this.btn_alterar.Location = new System.Drawing.Point(190, 274);
+            this.btn_alterar.Name = "btn_alterar";
+            this.btn_alterar.Size = new System.Drawing.Size(70, 52);
+            this.btn_alterar.TabIndex = 31;
+            this.btn_alterar.Text = "Alterar";
+            this.btn_alterar.UseVisualStyleBackColor = true;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(109, 274);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(70, 52);
+            this.btn.TabIndex = 30;
+            this.btn.Text = "Consultar";
+            this.btn.UseVisualStyleBackColor = true;
+            // 
+            // btn_inserir
+            // 
+            this.btn_inserir.Location = new System.Drawing.Point(28, 274);
+            this.btn_inserir.Name = "btn_inserir";
+            this.btn_inserir.Size = new System.Drawing.Size(70, 52);
+            this.btn_inserir.TabIndex = 29;
+            this.btn_inserir.Text = "Inserir";
+            this.btn_inserir.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "UF";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 149);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Cidade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Telefone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Nome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Id";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(104, 116);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(135, 20);
+            this.txt_email.TabIndex = 22;
+            // 
+            // txt_Cidade
+            // 
+            this.txt_Cidade.Location = new System.Drawing.Point(104, 142);
+            this.txt_Cidade.Name = "txt_Cidade";
+            this.txt_Cidade.Size = new System.Drawing.Size(83, 20);
+            this.txt_Cidade.TabIndex = 21;
+            // 
+            // txt_Id
+            // 
+            this.txt_Id.Location = new System.Drawing.Point(104, 38);
+            this.txt_Id.Name = "txt_Id";
+            this.txt_Id.Size = new System.Drawing.Size(33, 20);
+            this.txt_Id.TabIndex = 20;
+            // 
+            // txt_UF
+            // 
+            this.txt_UF.Location = new System.Drawing.Point(104, 168);
+            this.txt_UF.Name = "txt_UF";
+            this.txt_UF.Size = new System.Drawing.Size(33, 20);
+            this.txt_UF.TabIndex = 19;
+            // 
+            // txt_telefone
+            // 
+            this.txt_telefone.Location = new System.Drawing.Point(104, 90);
+            this.txt_telefone.Name = "txt_telefone";
+            this.txt_telefone.Size = new System.Drawing.Size(83, 20);
+            this.txt_telefone.TabIndex = 18;
+            // 
+            // txt_Nome
+            // 
+            this.txt_Nome.Location = new System.Drawing.Point(104, 64);
+            this.txt_Nome.Name = "txt_Nome";
+            this.txt_Nome.Size = new System.Drawing.Size(104, 20);
+            this.txt_Nome.TabIndex = 17;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_excluir);
+            this.Controls.Add(this.btn_alterar);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.btn_inserir);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.txt_Cidade);
+            this.Controls.Add(this.txt_Id);
+            this.Controls.Add(this.txt_UF);
+            this.Controls.Add(this.txt_telefone);
+            this.Controls.Add(this.txt_Nome);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_excluir;
+        private System.Windows.Forms.Button btn_alterar;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button btn_inserir;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.TextBox txt_Cidade;
+        private System.Windows.Forms.TextBox txt_Id;
+        private System.Windows.Forms.TextBox txt_UF;
+        private System.Windows.Forms.TextBox txt_telefone;
+        private System.Windows.Forms.TextBox txt_Nome;
+    }
+}
